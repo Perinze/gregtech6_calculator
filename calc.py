d67ef12e623eca6fd375056705a61b9bd7173254
perinze <perinzefaper@foxmail.com>
2020-12-15 12:02:29 +0800

    rewrite plus() to merge()

--- calc.py
+++ calc.py
@@ -1,59 +1,46 @@
 import json
 
 
-def plus(dict_a, dict_b, cnt):
-	for bk in dict_b.keys():
-		sub_dict_b = dict_b[bk]
-
-		if bk in dict_a.keys():
-			sub_dict_a = dict_a[bk]
-
-			for sk in sub_dict_b.keys():
-				if sk in sub_dict_a.keys():
-					new_val = sub_dict_a[sk] + sub_dict_b[sk] * cnt
-				else:
-					new_val = sub_dict_b[sk] * cnt
-				dict_a[bk].update({sk: new_val})
-
+# multiply leaves of dict tree
+def mul(d, cnt):
+	for k in d.keys():
+		if type(d[k]) == int:
+			d[k] *= cnt
 		else:
-			for sk in sub_dict_b.keys():
-				new_val = sub_dict_b[sk] * cnt
-				dict_a.update({bk: {sk: new_val}})
+			mul(d[k], cnt)
 
 
+def merge(da, db, cnt):
+	for k in db.keys():
+		if (type(db[k]) == int):
+			if k in da.keys():
+				da[k] += db[k] * cnt
+			else:
+				da[k] = db[k] * cnt
+		else:
+			if k in da.keys():
+				merge(da[k], db[k], cnt, rank + 1)
+			else:
+				dbc = db
+				mul(dbc, cnt)
+				da[k] = db[k]
+				
+				
 def calc(name, cnt, mat='', rank=0):
-	log = open("calc.log", "w")
-
-	log.write("{}calc('{}', {}, '{}')".format(rank * '\t', name, cnt, mat))
 	with open("craft/{}.json".format(name), 'r') as f:
 		craft = json.loads(f.read())
 	f.close()
 
 	if len(craft) == 0:
-		return {name: {mat: cnt}}	# {plate: {"": 4}}
+		return {name: {mat: cnt}}
 	
 	res = {}
-	for meta in craft.keys():	# for key in {curved_plate, ring, screw}
+	for meta in craft.keys():
 		for material in craft[meta].keys():
 			cover_material = material
 			if cover_material == '':
 				cover_material = mat
-			subres = calc(meta, craft[meta][material], cover_material, rank + 1)	# calc(curved_plate, {"": 4}), subres = {plate: {"": 4}}
-			plus(res, subres, cnt)
+			subres = calc(meta, craft[meta][material], cover_material, rank + 1)
+			merge(res, subres, cnt)
 
-		'''
-		for meta in subres.keys():	# plate, ring, screw
-			for material in subres[meta].keys():	# "", stainless_steel, invar
-				if meta in res.keys():	# plate in res.keys = [plate, ring, screw]
-					if material in res[meta].keys():	# stainless_steel in ["", "stainless_steel"]
-						res[meta][material] += subres[meta][meterial] * cnt
-					else:
-						res[meta][material] = subres[meta][meterial] * cnt
-				else:
-				 	res[meta][material] = subres[meta][material] * cnt
-		'''
-
-	log.write("{}res = {}".format(rank * '\t', res))
-
-	log.close()
 	return res
